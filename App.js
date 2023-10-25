97ab3ea5d04e6881a0c56f579afeca4a12cbfa0d
Elijah Frempong <elijahfrimpong76@gmail.com>
2023-10-25 15:36:16 +0000

    first app

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.imageContainer}>
+      <Image source={PlaceholderImage} style={styles.image}/>
+    </View>
+      {/* <Text style = {{color : '#fff'}}>Open up App.js to start working on your app!</Text> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 20,
+  },
+  image: {
+    width: 320,
+    height: 450,
+    borderRadius: 18,
+  },
+
 });
